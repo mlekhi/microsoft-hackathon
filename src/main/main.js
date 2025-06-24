8ab1497435392bd99ae2b346621822d6c4e053ef
mlekhi <maya.lekhi1@gmail.com>
2025-06-24 13:15:03 -0700

    Clean up unnecessary files and dependencies

--- src/main/main.js
+++ src/main/main.js
@@ -1,12 +1,12 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, '../preload/preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
     },
   });
 
